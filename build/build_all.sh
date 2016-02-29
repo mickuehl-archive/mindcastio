5775eb545414630f54f58163ca937ef1846ce614
Michael Kuehl <michael@ratchet.cc>
2016-02-29 17:32:32 +0100

    changes to the build workflow

--- build/build_all.sh
+++ build/build_all.sh
@@ -1,15 +1,23 @@
 #!/bin/bash
 
-# updated dependencies
-# go get -u "github.com/mindcastio/podcast-feed"
-# go get -u "github.com/mindcastio/go-json-rest"
-
 # build binaries
-cd $MINDCASTIO_HOME/services/crawler
-go build
+cd $MINDCAST_SRC/services/crawler
+go get && go build
 
-cd $MINDCASTIO_HOME/services/indexer
-go build
+cd $MINDCAST_SRC/services/indexer
+go get && go build
 
-cd $MINDCASTIO_HOME/services/search
-go build
+cd $MINDCAST_SRC/services/search
+go get && go build
+
+if [ ! -L "/usr/local/bin/mindcast-crawler" ]; then
+	sudo ln -s "$MINDCAST_SRC/services/crawler/crawler" /usr/local/bin/mindcast-crawler
+fi
+
+if [ ! -L "/usr/local/bin/mindcast-indexer" ]; then
+	sudo ln -s "$MINDCAST_SRC/services/indexer/indexer" /usr/local/bin/mindcast-indexer
+fi
+
+if [ ! -L "/usr/local/bin/mindcast-search" ]; then
+	sudo ln -s "$MINDCAST_SRC/services/search/search" /usr/local/bin/mindcast-search
+fi
