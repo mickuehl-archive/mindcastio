a871019b5fd7fe527e55721c099097a82f5d2be7
Michael Kuehl <michael@ratchet.cc>
2016-03-02 19:02:47 +0100

    removed nats from the build script

--- build/docker.sh
+++ build/docker.sh
@@ -1,17 +1,15 @@
 #!/bin/bash
 
-docker stop natsd
 docker stop mongod
 docker stop elasticd
 
-docker rm natsd mongod elasticd
+docker rm mongod elasticd
 
 docker pull mongo
 docker pull elasticsearch
-docker pull nats
 
-docker create --name mongod -p $PRIVATE_IPV4:27017:27017 mongo --storageEngine=wiredTiger
-docker create --name elasticd -p $PRIVATE_IPV4:9200:9200 -p $PRIVATE_IPV4:9300:9300 elasticsearch
-docker create --name natsd -p $PRIVATE_IPV4:4222:4222 -p 6222:6222 nats
+docker create --name mongod -p $PRIVATE_IP4:27017:27017 mongo --storageEngine=wiredTiger
+docker create --name elasticd -p $PRIVATE_IP4:9200:9200 -p $PRIVATE_IP4:9300:9300 elasticsearch
+#docker create --name natsd -p $PRIVATE_IPV4:4222:4222 -p 6222:6222 nats
 
-docker start natsd mongod elasticd
+docker start mongod elasticd
